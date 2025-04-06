44ab559909e5cfc420b10ff067c339094e9da5b7
Harvey <ngha@oregonstate.edu>
2025-04-05 21:02:37 -0400

    Lex Modio Harvey Yasser: index.j2 displays response from Gemini API

--- MinervAI/app.py
+++ MinervAI/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, json, redirect, request
+from gemini_test import generate
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=["GET", "POST"])
+def home():
+    if request.method == "GET":
+        return render_template("index.j2")
+    
+    if request.method == "POST":
+        if request.form.get('userInput'):
+            userInputData = request.form['userInput']
+
+            gemini_response = generate(userInputData)
+            
+        return render_template("index.j2", gemini_response=gemini_response)
+
+#@app.route('/summarize', methods = ['POST'])
+
 
 if __name__ == "__main__":
     #Start app on port 56983, will be different once hosted
